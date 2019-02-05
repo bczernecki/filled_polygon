83a052d2e1d0b913b17052a96db92af031ece439
jn <tupiszakaczki@gmail.com>
2019-02-05 17:48:22 +0100

    moves from PL to ENG

--- piotr.R
+++ piotr.R
@@ -16,8 +16,7 @@ head(res)
 res$anomaly <- ifelse(res$TEMP - res$AVE > 0, 1, -1)
 res$chunk <- c(1, cumsum(abs(diff(res$anomaly))) / 2 + 1) # first element needs to be added
 
-res$kolorki <- ifelse(res$anomaly > 0, "red", "blue")
-#res <- res[1:100,]
+res$colors <- ifelse(res$anomaly > 0, "red", "blue")
 
 l <- split(res, res$chunk)
 
@@ -26,71 +25,71 @@ for (i in seq_along(l)){
     # to avoid calculating for the last element
     n <- nrow(l[[i]])
     y <- c(l[[i]]$TEMP[n], l[[i + 1]]$TEMP[1])
-    x <- c(l[[i]]$DATA[n], l[[i + 1]]$DATA[1])
+    x <- c(l[[i]]$DATE[n], l[[i + 1]]$DATE[1])
     
-    temp_pomiedzy <- mean(c(l[[i]]$AVE[n], l[[i + 1]]$AVE[1]))
+    temp_between <- mean(c(l[[i]]$AVE[n], l[[i + 1]]$AVE[1]))
     aprox <- approx(x = x, y = y, n = 100)
-    ind <- which.min(abs(aprox$y - temp_pomiedzy))
+    ind <- which.min(abs(aprox$y - temp_between))
     
     l[[i]] <- rbind(l[[i]], l[[i]][n, ])
-    l[[i]]$DATA[n + 1] <- aprox$x[ind]
+    l[[i]]$DATE[n + 1] <- aprox$x[ind]
     l[[i]]$AVE[n + 1] <- aprox$y[ind]
     l[[i]]$TEMP[n + 1] <- aprox$y[ind]
     print(i)
     
     l[[i + 1]] <- rbind(l[[i + 1]][1, ], l[[i + 1]])
-    l[[i + 1]]$DATA[1] <- aprox$x[ind]
+    l[[i + 1]]$DATE[1] <- aprox$x[ind]
     l[[i + 1]]$AVE[1] <- aprox$y[ind]
     l[[i + 1]]$TEMP[1] <- aprox$y[ind]
-    l[[i + 1]]$kolorki <- as.character(l[[i + 1]]$kolorki)
+    l[[i + 1]]$colors <- as.character(l[[i + 1]]$colors)
   }
 }
 
-plot(res$DATA, res$AVE, bty = "L", col = "black", type = "l", 
+plot(res$DATE, res$AVE, bty = "L", col = "black", type = "l", 
      ylab = "Temperature anomalies (wrt. 1981-2010)", xlab = "", cex.lab = 1.2, 
      cex.axis = 1.2, cex.main = 1.5, ylim = c(-21.5, 28), xaxs = "i", 
      xaxt = "n", yaxs = "i", main = "2018")
-res$DATA <-  (as.Date(res$DATA, origin = "1970-01-01"))
-lines(res$DATA, res$AVE, lwd = 2)
-axis(1, at = res$DATA[which(format(res$DATA, "%d") == "01")],
-     labels = format(res$DATA[which(format(res$DATA, "%d") == "01")], "%b"))
-abline(v = res$DATA[which(format(res$DATA, "%d") == "01")], col = "gray")
+res$DATE <-  (as.Date(res$DATE, origin = "1970-01-01"))
+lines(res$DATE, res$AVE, lwd = 2)
+axis(1, at = res$DATE[which(format(res$DATE, "%d") == "01")],
+     labels = format(res$DATE[which(format(res$DATE, "%d") == "01")], "%b"))
+abline(v = res$DATE[which(format(res$DATE, "%d") == "01")], col = "gray")
 abline(h = -10:10 * 5, col = "gray")
-lines(res$DATA, res$MAX, lty = 2, col = "red")
-lines(res$DATA, res$MIN, lty = 2, col = "blue")
-lapply(l, function(x) polygon(c(x$DATA, rev(x$DATA)),
+lines(res$DATE, res$MAX, lty = 2, col = "red")
+lines(res$DATE, res$MIN, lty = 2, col = "blue")
+lapply(l, function(x) polygon(c(x$DATE, rev(x$DATE)),
                               c(x$TEMP, rev(x$AVE)),
-                              col = x$kolorki))
-legend(x = res$DATA[10], y = 25, 
+                              col = x$colors))
+legend(x = res$DATE[10], y = 25, 
        legend = c(2018, 2018, "MAX 1950-2017", "MIN 1950-2017"), 
        cex = 0.8, lty = c(1, 1, 2, 2), lwd = c(4, 4, 1, 1), 
        col = c("red", "blue", "red", "blue"), bg = "#FFFFFF90")
-points(x = res$DATA[res$TEMP > res$MAX], 
-       y = rep(-21, length(res$DATA[res$TEMP > res$MAX])), 
+points(x = res$DATE[res$TEMP > res$MAX], 
+       y = rep(-21, length(res$DATE[res$TEMP > res$MAX])), 
        pch = 15, lwd = 5, col = "red")
-points(x = res$DATA[res$TEMP < res$MIN], 
-       y = rep(-21, length(res$DATA[res$TEMP < res$MIN])),
+points(x = res$DATE[res$TEMP < res$MIN], 
+       y = rep(-21, length(res$DATE[res$TEMP < res$MIN])),
        pch = 15, lwd = 5, col = "blue")
 
-iledni <- res %>%
-  mutate(year = format(DATA, "%Y")) %>%
+nr_of_days <- res %>%
+  mutate(year = format(DATE, "%Y")) %>%
   filter(year == "2018") %>%
-  group_by(format(DATA, "%Y-%m-01"), as.factor(anomaly)) %>%
+  group_by(format(DATE, "%Y-%m-01"), as.factor(anomaly)) %>%
   summarise(suma = sum(anomaly))
-head(iledni)
+head(nr_of_days)
 
-colnames(iledni) <- c("data", "anom", "suma")
-iledni$data <- as.Date(iledni$data)
-iledni$anom <- as.numeric(as.character(iledni$anom))
-head(iledni)
+colnames(nr_of_days) <- c("data", "anom", "suma")
+nr_of_days$data <- as.Date(nr_of_days$data)
+nr_of_days$anom <- as.numeric(as.character(nr_of_days$anom))
+head(nr_of_days)
 
-miny <- iledni[iledni$anom == -1, ] %>% as.data.frame()
-maxy <- iledni[iledni$anom == 1, ] %>% as.data.frame()
-text(miny$data + 8,  26,  labels = abs(miny$suma), col = "blue", cex = 0.8)
-text(maxy$data + 22,  26,  labels = abs(maxy$suma), col = "red", cex = 0.8)
+miny <- nr_of_days[nr_of_days$anom == -1, ] %>% as.data.frame()
+maxy <- nr_of_days[nr_of_days$anom == 1, ] %>% as.data.frame()
+text(miny$data + 8,  26, labels = abs(miny$suma), col = "blue", cex = 0.8)
+text(maxy$data + 22,  26, labels = abs(maxy$suma), col = "red", cex = 0.8)
 
-title(main = NULL, sub = paste0("liczba dni < średniej:  ", abs(sum(miny$suma))), 
+title(main = NULL, sub = paste0("number of days < average:  ", abs(sum(miny$suma))), 
       col.sub = "blue", adj = 1) 
-title(main = NULL, sub = paste0("\nliczba dni > średniej:  ", sum(maxy$suma)), 
+title(main = NULL, sub = paste0("\nnumber of days > average:  ", sum(maxy$suma)), 
       col.sub = "red", adj = 0)
 
